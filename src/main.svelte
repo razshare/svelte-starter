30d89a34d07400feec4983086b6f3a6e3ab5c36d
Razvan Tanase <tangent.jotey@gmail.com>
2023-02-20 15:52:00 +0100

    prettier configuration added and ts changes

--- src/main.svelte
+++ src/main.svelte
@@ -1,25 +1,23 @@
 <script lang="ts">
-  import { listen, register } from "@scripts/push-notifications";
-  import { PushNotifications } from "@capacitor/push-notifications";
-  import { SplashScreen } from "@capacitor/splash-screen";
-  import { onMount } from "svelte";
+  import { SplashScreen } from '@capacitor/splash-screen'
+  import { listen, register } from '@scripts/push-notifications'
+  import { onMount } from 'svelte'
 
-  if(window.cordova){
-    onMount(SplashScreen.hide);
+  if (window.cordova) {
+    onMount(SplashScreen.hide)
     onMount(async () => {
       if (await register()) {
         listen({
-          onregistration: (token) => console.log("token", { token }),
-          onregistrationerror: (error) => console.log("token", { error }),
-          onaction: (action) => console.log("action", { action }),
-          onnotification: (notification) =>
-            console.log("notification", { notification }),
-        });
+          onregistration: token => console.log('token', { token }),
+          onregistrationerror: error => console.log('token', { error }),
+          onaction: action => console.log('action', { action }),
+          onnotification: notification => console.log('notification', { notification }),
+        })
       }
-    });
+    })
   }
 
-  let counter = 0;
+  let counter = 0
 </script>
 
 <div class="grid justify-center content-center h-full w-full">
