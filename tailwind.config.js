7de8b96dcc64208a34aba7f7550594a6bc8b1df8
tncrazvan <tangent.jotey@gmail.com>
2023-06-28 01:24:56 +0200

    switching to skeletonui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,19 @@
-import daisyui from 'daisyui'
+import skeletonui from '@skeletonlabs/skeleton/tailwind/skeleton.cjs'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{svelte,js,ts}'],
-	plugins: [daisyui],
-  daisyui: {
-    styled: true,
-    themes: true,
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-    prefix: '',
-    themes: [
-      {
-        custom: {
-          ...require('daisyui/src/colors/themes')['[data-theme=dark]'],
-        },
-      },
-    ],
-  },
+	// 1. Apply the dark mode class setting:
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		// 2. Append the path for the Skeleton NPM package and files:
+		require('path').join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}'),
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		// 3. Append the Skeleton plugin to the end of this list
+		...skeletonui(),
+	],
 }
