a9b98d5dc96dce3811cf515404db5d31bf0c4f76
tncrazvan <tangent.jotey@gmail.com>
2023-06-27 22:53:02 +0200

    updating aliases

--- src/main.svelte
+++ src/main.svelte
@@ -1,10 +1,10 @@
 <script>
-	import DefaultPage from '$pages/default-page.svelte'
+	import HomePage from '@pages/home-page.svelte'
 	import { Route, Router } from 'svelte-routing'
 </script>
 
 <div class="grid justify-center content-center h-full w-full">
 	<Router>
-		<Route path="*" component={DefaultPage} />
+		<Route path="*" component={HomePage} />
 	</Router>
 </div>
