bc2a632da8ed724a9700de1999d751433cbad27d
Razvan <tangent.jotey@gmail.com>
2023-09-23 00:37:50 +0200

    adding :main alias

--- vite.config.js
+++ vite.config.js
@@ -7,44 +7,45 @@ const dir = path.dirname(file).replace(/\\+/, '/')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [svelte()],
-	resolve: {
-		alias: {
-			':pages': `${path.resolve(dir, './src/lib/:pages')}`,
-			':components': `${path.resolve(dir, './src/lib/:components')}`,
-			':stores': `${path.resolve(dir, './src/lib/:stores')}`,
-			':types': `${path.resolve(dir, './src/lib/:types')}`,
-			':scripts': `${path.resolve(dir, './src/lib/:scripts')}`,
-			':constants': `${path.resolve(dir, './src/lib/:constants.js')}`,
-			':assets': `${path.resolve(dir, './:assets')}`,
-		},
-	},
-	optimizeDeps: {
-		exclude: ['svelte-routing'],
-	},
-	build: {
-		sourcemap: true,
-		outDir: 'dist',
-		minify: false,
-		emptyOutDir: true,
-		rollupOptions: {
-			output: {
-				entryFileNames: `assets/[name].js`,
-				chunkFileNames: `assets/[name].js`,
-				assetFileNames: `assets/[name].[ext]`,
-			},
-		},
-	},
-	server: {
-		https: false,
-		host: '::',
-		proxy: {
-			'^/api/.*': {
-				target: 'http://127.0.0.1:8000',
-				changeOrigin: false,
-				secure: false,
-				ws: true,
-			},
-		},
-	},
+  plugins: [svelte()],
+  resolve: {
+    alias: {
+      ':pages': `${path.resolve(dir, './src/lib/:pages')}`,
+      ':components': `${path.resolve(dir, './src/lib/:components')}`,
+      ':stores': `${path.resolve(dir, './src/lib/:stores')}`,
+      ':types': `${path.resolve(dir, './src/lib/:types')}`,
+      ':scripts': `${path.resolve(dir, './src/lib/:scripts')}`,
+      ':assets': `${path.resolve(dir, './:assets')}`,
+      ':constants': `${path.resolve(dir, './src/lib/:constants.js')}`,
+      ':main': `${path.resolve(dir, './src/:main.js')}`,
+    },
+  },
+  optimizeDeps: {
+    exclude: ['svelte-routing'],
+  },
+  build: {
+    sourcemap: true,
+    outDir: 'dist',
+    minify: false,
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
+  server: {
+    https: false,
+    host: '::',
+    proxy: {
+      '^/api/.*': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: false,
+        secure: false,
+        ws: true,
+      },
+    },
+  },
 })
