9fbbd068d93833a907f10b9feb7a18947cc266fd
Razvan <tangent.jotey@gmail.com>
2023-09-23 00:40:58 +0200

    adding themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ import daisyui from 'daisyui'
 
 /** @type {import('daisyui').Config} */
 export default {
-	content: ['./src/**/*.{html,js,ts,svelte}'],
-	plugins: [daisyui],
+  content: ['./src/**/*.{html,js,ts,svelte}'],
+  plugins: [daisyui],
+  daisyui: {
+    themes: true,
+  },
 }
